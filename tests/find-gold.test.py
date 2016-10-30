a86960b0a597a92517d1dcddbd53ac7268b2c94c
Paul Brodsky <paul.brodsky@gmail.com>
2016-10-30 14:49:02 -0400

    adding first test

---
+++ tests/find-gold.test.py
@@ -0,0 +1,15 @@
+sandwich = ['bread', 'lettuce','cheese', 'meat', 'bread']
+
+# TODO add code to remove the lettuce
+# hint: google how to remove an item from a list in python
+#####################
+
+
+
+
+#####################
+
+if 'lettuce' in sandwich:
+  print 'Make a yucky face'
+else:
+  print 'Shove the sandwich into your mouth with both hands. If you run out of room, wait for a moment to let your saliva moisten the payload and then use your fingers to press the contents further to make some room.'
