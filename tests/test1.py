ca1b097b2d9964f0e329c363b5223d4bbbb4bbf9
Benjamin Brodsky <b3nb0b@gmail.com>
2016-11-09 16:25:44 -0500

    There

--- tests/test1.py
+++ tests/test1.py
@@ -13,3 +13,4 @@ if 'lettuce' in sandwich:
   print 'Make a yucky face'
 else:
   print 'Shove the sandwich into your mouth with both hands. If you run out of room, wait for a moment to let your saliva moisten the payload and then use your fingers to press the contents further to make some room.'
+ while 'lettuce' in : sandwich.remove('lettuce')
