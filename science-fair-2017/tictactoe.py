61931b01a510d20b2bd4425fd43482a0b4906d63
Paul Brodsky <paul.brodsky@gmail.com>
2017-01-18 08:10:48 +0000

    cleaning up

--- science-fair-2017/tictactoe.py
+++ science-fair-2017/tictactoe.py
@@ -4,17 +4,16 @@
 #testing cloud9
 
 
-sqaures = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+squares = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 print 'Instructions: these are the rules'
 
 player = 'X'
 
 def printBoard():
-    print sqaures[0] + '|' + sqaures[1] + '|' + sqaures[2]
-    print sqaures[3] + '|' + sqaures[4] + '|' + sqaures[5]
-    print sqaures[6] + '|' + sqaures[7] + '|' + sqaures[8]
-    
+    print squares[0] + '|' + squares[1] + '|' + squares[2]
+    print squares[3] + '|' + squares[4] + '|' + squares[5]
+    print squares[6] + '|' + squares[7] + '|' + squares[8]
 
 while True:
     printBoard()
@@ -22,7 +21,7 @@ while True:
     number = input("what's your move, " + player + "?")
     number = number - 1
 
-    sqaures[number] = player
+    squares[number] = player
 
     if player == 'X':
         player = 'O'
