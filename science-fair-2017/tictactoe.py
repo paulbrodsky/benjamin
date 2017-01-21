1275ca1cd3783b5ffc99e08e2dff8a4de3a15da5
Paul Brodsky <paul.brodsky@gmail.com>
2017-01-21 22:46:06 +0000

    we got a working game woo

--- science-fair-2017/tictactoe.py
+++ science-fair-2017/tictactoe.py
@@ -1,17 +1,33 @@
-squares = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+#these are the functions
 
+def areSquaresMarked(player, f, s, t):
+    return squares[f] == player and squares[s] == player and squares[t] == player
+
+def isWinner(player, squares):
+    return areSquaresMarked(player, 0, 1, 2) \
+    or areSquaresMarked(player, 3, 4, 5) \
+    or areSquaresMarked(player, 6, 7, 8) \
+    or areSquaresMarked(player, 0, 4, 8) \
+    or areSquaresMarked(player, 2, 4, 6) \
+    or areSquaresMarked(player, 0, 3, 6) \
+    or areSquaresMarked(player, 1, 4, 7) \
+    or areSquaresMarked(player, 2, 5, 8)
+
+def printBoard(squares):
+    print squares[0] + '|' + squares[1] + '|' + squares[2]
+    print squares[3] + '|' + squares[4] + '|' + squares[5]
+    print squares[6] + '|' + squares[7] + '|' + squares[8]
+    
+# this is the game
+squares = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
 print 'Instructions: these are the rules'
 
 player = 'X'
 
-def printBoard():
-    print squares[0] + '|' + squares[1] + '|' + squares[2]
-    print squares[3] + '|' + squares[4] + '|' + squares[5]
-    print squares[6] + '|' + squares[7] + '|' + squares[8]
-
 while True:
-    printBoard()
-   
+    printBoard(squares)
+    
     number = raw_input("what's your move, " + player + "?")
     
     # if the value of the number string is not numeric 
@@ -32,8 +48,15 @@ while True:
         
     number = number - 1
     squares[number] = player
-
+    
+    if isWinner(player, squares):
+        print 'you won'
+        printBoard()
+        break
+    
     if player == 'X':
         player = 'O'
     else:
         player = 'X'
+  
+        
