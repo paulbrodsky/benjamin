8f6785481dcdd0a4f22755a1590e02534d02db51
baby ben brodsky <baby ben brodsky>
2017-01-29 15:31:07 -0500

    got the loop working

--- science-fair-2017/hangman.py
+++ science-fair-2017/hangman.py
@@ -1,11 +1,22 @@
 word = 'apple'
-letter = raw_input("What letter would you like to guess?")
-wordLetters = []
 
-if letter in word:
-    print "you got it right"
-else:
-    print "you got it wrong"
+hits = []
+misses = []
 
-wordLetters.extend(letter)
-print wordLetters
+while True: 
+    letter = raw_input("What letter would you like to guess?")
+    if letter in word:
+        print "you got it right"
+        hits.extend(letter)
+        print hits
+    else:
+        print "you got it wrong"
+        misses.extend(letter)
+        print misses
+
+
+
+
+
+
+
