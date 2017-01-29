ea44068b6038e994b94352e27479e204e9fef0e8
baby ben brodsky <baby ben brodsky>
2017-01-29 15:59:45 -0500

    added winning system

--- science-fair-2017/hangman.py
+++ science-fair-2017/hangman.py
@@ -1,19 +1,42 @@
-word = 'apple'
 
-hits = []
-misses = []
+def isWinner(hits, word):
+    for letter in word:
+        if letter not in hits:
+            return False
+    return True
+    
+    
+    
 
-while True: 
-    letter = raw_input("What letter would you like to guess?")
-    if letter in word:
-        print "you got it right"
-        hits.extend(letter)
-        print hits
-    else:
-        print "you got it wrong"
-        misses.extend(letter)
-        print misses
+words = ["apple", "bannana", "mango"]
 
+for word in words:
+    
+    hits = []
+    misses = []
+
+    while True: 
+
+        print "Starting new game"
+
+        letter = raw_input("What letter would you like to guess?")
+        
+        if letter in hits or letter in misses:
+            print "You already guessed that!"
+            continue
+
+        if letter in word:
+            print "you got it right"
+            hits.extend(letter)
+            print hits
+        else:
+            print "you got it wrong"
+            misses.extend(letter)
+            print misses
+
+        if isWinner(hits, word):
+            print "You won!"
+            break
 
 
 
