09e19367bd390f01fab39e30ed871c3793e34cab
Ben Brodsky <b3nb0b@gmail.com>
2020-06-18 00:32:20 -0400

    able to read text from .txt files

--- subcipher/Program.cs
+++ subcipher/Program.cs
@@ -4,10 +4,14 @@ namespace subcipher
 {
     class Program
     {
+        private const string Format = "{0}";
+        private const string Path = @"C:\Users\b3nb0\Downloads\Encryption\Message.txt";
+
         static void Main(string[] args)
         {
             if (args[0] == "encrypt"){
-                Console.WriteLine("it worked");
+                  string text = System.IO.File.ReadAllText(Path);
+                  System.Console.WriteLine(Format, text);
             } 
             else{
                 System.Console.WriteLine("it didn't work");
