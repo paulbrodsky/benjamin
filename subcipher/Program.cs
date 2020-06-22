58f2ce3dcea8e59aa7c36913d534b53349e6198c
Ben Brodsky <b3nb0b@gmail.com>
2020-06-22 00:16:58 -0400

    added map dictionary

--- subcipher/Program.cs
+++ subcipher/Program.cs
@@ -1,22 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace subcipher
 {
     class Program
     {
         private const string Format = "{0}";
-       
+
 
         static void Main(string[] args)
         {
-            if (args[0] == "encrypt"){
-                    string text = System.IO.File.ReadAllText(args[1]);
-                    System.Console.WriteLine(text);
-                    
-            } 
-            else{
+            var map = System.IO.File.ReadAllText(args[2]);
+
+            var mapItems = map.Split("\r\n");
+
+            var mapDictionary = new Dictionary<char, char>();
+
+            foreach (var mapItem in mapItems)
+            {
+                var mapPair = mapItem.Split("=");
+
+                mapDictionary.Add(mapPair[0][0], mapPair[1][0]);
+
+                System.Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+            if (args[0] == "encrypt")
+            {
+                string Message = System.IO.File.ReadAllText(args[1]);
+                var firstcharacter = Message[0];
+
+
+                foreach (char letter in Message)
+                {
+
+                }
+
+            }
+            else
+            {
                 System.Console.WriteLine("Message Not Found.");
             }
+
+
+
+
         }
     }
 }
