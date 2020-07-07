ae08acdda37872e19d2cbecca84db2b2781bcd91
Ben Brodsky <b3nb0b@gmail.com>
2020-07-07 15:16:23 -0400

    fixed empty line test

--- subcipher/lib/Subcipher.cs
+++ subcipher/lib/Subcipher.cs
@@ -28,7 +28,13 @@ namespace lib
             foreach (var mapItem in mapItems)
             {
                 var mapPair = mapItem.Split("=".ToCharArray());
-                mapDictionary.Add(mapPair[0][0], mapPair[1][0]);
+
+                if (mapPair.Length == 2)
+                {
+                    mapDictionary.Add(mapPair[0][0], mapPair[1][0]);
+                }
+
+
             }
 
             return mapDictionary;
