20f53ef194c18985928d63c77ef7edb8c3f088be
Paul Brodsky <paul.brodsky@gmail.com>
2020-07-12 18:49:27 -0400

    cleanup

--- subcipher/console/Program.cs
+++ subcipher/console/Program.cs
@@ -1,36 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace subcipher
 {
     class Program
     {
-        private const string Format = "{0}";
-
-
         static void Main(string[] args)
         {
-
-            try
-            {
-                string myString = "hello";
-
-                System.Console.WriteLine(myString.Length);
-                throw new Exception();
-            }
-            catch (System.NullReferenceException)
-            {
-                System.Console.WriteLine("caught a null reference exception");
-            }
-            catch (Exception exc)
-            {
-
-                System.Console.WriteLine("caught an exception");
-
-
-            }
-
-
         }
     }
 }
