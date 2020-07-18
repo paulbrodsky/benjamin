b09348505e7438c7d31ca1f2146fb03eb043586e
Ben Brodsky <b3nb0b@gmail.com>
2020-07-17 21:34:47 -0400

    fact error

--- Archive/leetcode/lletcodetest.cs
+++ Archive/leetcode/lletcodetest.cs
@@ -7,23 +7,24 @@ namespace leetcode
 {
     public class LeetCodeTests
     {
-        [Fact]
-        public void RemoveDuplicate()
-        {
-            var originalList = "3231214321";
+        // [Fact]
+        // public void RemoveDuplicate()
+        // {
+        //     var originalList = "3231214321";
 
-            var emptyList = string.Empty;
+        //     var emptyList = string.Empty;
 
-            foreach (char c in originalList)
-            {
-                if (!emptyList.Contains(c))
-                {
-                    emptyList = emptyList + c;
-                }
-            }
+        //     foreach (char c in originalList)
+        //     {
+        //         if (!emptyList.Contains(c))
+        //         {
+        //             emptyList = emptyList + c;
+        //         }
+        //     }
 
-            Assert.Equal("3214", emptyList);
-        }
+        // //     Assert.Equal("3214", emptyList);
+        // // }
+
 
         int[] arr = { 10, 324, 45, 90, 9808 };
 
@@ -46,6 +47,13 @@ namespace leetcode
 
             return max;
         }
+
+        // public void findHighestNumber()
+        // {
+
+        // }
+
+
     }
 }
 
