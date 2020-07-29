f5ac6d068909a55989016e3efd2f9e4faf81a66f
Ben Brodsky <b3nb0b@gmail.com>
2020-07-29 16:34:00 -0400

    remove vowels test

---
+++ Challenges/RemoveVowels.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using Xunit;
+using System.Collections.Generic;
+
+namespace Challanges
+{
+    public class RemoveVowels
+    {
+        [Fact]
+        public void Run()
+        {
+            var vowels = new string[] { "a", "e", "i", "o", "u" };
+
+            string message = "hello world";
+
+            var editedMessage = this.VowelRemove(vowels, message);
+
+            Assert.Equal(editedMessage, "hll wrld");
+        }
+
+        public string VowelRemove(string[] vowels, string message)
+        {
+            string output = string.Empty;
+
+            for (int i = 0; i < vowels.Length; i++)
+            {
+                message = message.Replace(vowels[i], string.Empty);
+            }
+
+            return message;
+        }
+
+    }
+}
+
+
+
