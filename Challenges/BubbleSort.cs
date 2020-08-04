9d0370a4b4b81b01bca83411b43684f8f3c248d9
Ben Brodsky <b3nb0b@gmail.com>
2020-08-04 18:25:39 -0400

    bubble sort test

--- Challenges/BubbleSort.cs
+++ Challenges/BubbleSort.cs
@@ -15,9 +15,33 @@ namespace Challanges
             Assert.Equal(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, input);
         }
 
-        // your code goes here
+        public void Sort(int[] input)
+        {
+
+            bool hasSwapped = true;
+
+            do
+            {
+                hasSwapped = false;
+
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    if (input[i] > input[i + 1])
+                    {
+                        var temp = input[i];
+
+                        input[i] = input[i + 1];
+
+                        input[i + 1] = temp;
+
+                        hasSwapped = true;
+
+                    }
+
+                }
+            } while (hasSwapped);
+        }
     }
 }
-
 
 
